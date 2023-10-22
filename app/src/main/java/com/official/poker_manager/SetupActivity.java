0421cab3df15d91d247c3e70ac021afb62e5fb0d
Marcos-Campanella <mhmcamp@gmail.com>
2023-10-22 17:20:48 -0300

    Initial Commit

---
+++ app/src/main/java/com/official/poker_manager/SetupActivity.java
@@ -0,0 +1,14 @@
+package com.official.poker_manager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SetupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setup);
+    }
+}
