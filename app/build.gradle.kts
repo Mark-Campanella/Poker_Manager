066fa15820689b955b30ffab50c5b98223f4d0a2
Arte-Darc <andre.lisboa@aluno.ifsp.edu.br>
2023-10-22 17:58:22 -0300

    compileSDK 33 -> 34
    Atualização do Gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.official.poker_manager"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.official.poker_manager"
