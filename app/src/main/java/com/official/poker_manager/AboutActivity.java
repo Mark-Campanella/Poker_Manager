1929196fb05bfc425fee1158d3b6f1572ddeefd8
Marcos-Campanella <mhmcamp@gmail.com>
2023-11-03 12:05:00 -0300

    tela do "sobre"

--- app/src/main/java/com/official/poker_manager/AboutActivity.java
+++ app/src/main/java/com/official/poker_manager/AboutActivity.java
@@ -3,12 +3,36 @@ package com.official.poker_manager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 
 public class AboutActivity extends AppCompatActivity {
-
+    public TextView text_about;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        text_about = (TextView) findViewById(R.id.txt_about);
+        //criando um objeto do tipo inputstream
+        InputStream inputStream = this.getResources().openRawResource(R.raw.about);
+        //criando um objeto de buffer
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        //criando o objeto de Buffer de String
+        StringBuffer stringBuffer = new StringBuffer();
+        //passando do buffer para o bueffer de string
+        String strData = "";
+        if (inputStream!=null) try {
+            while ((strData = bufferedReader.readLine()) != null) {
+                stringBuffer.append(strData+"\n");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        text_about.setText(stringBuffer);
     }
 }
