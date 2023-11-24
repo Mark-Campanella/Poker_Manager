8f6fc5deb502c7b3c1b97dce7d108cdc0717500c
FilhoDeEd <edson.er.rodrigues@gmail.com>
2023-11-23 21:46:19 -0300

    Adição de um método que inicializa a vizualização do jogo startGameActivity.

--- app/src/main/java/com/official/poker_manager/GameActivity.java
+++ app/src/main/java/com/official/poker_manager/GameActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,9 +14,11 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.Random;
 
 public class GameActivity extends AppCompatActivity {
     private Game game;
+    private ArrayList<Player> players;
     private final Hashtable<Integer, SeatViews> seatViewsMap = new Hashtable<Integer, SeatViews>(10);
     private class SeatViews
     {
@@ -30,31 +34,14 @@ public class GameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_game);
 
         // Recuperando o objeto Game criado em SetupActivity
-        if (getIntent().getExtras() != null)
+        if(getIntent().getExtras() != null)
         {
             game = (Game) getIntent().getSerializableExtra("game");
         }
 
-        // Faz o bind de todas as views
-        for(int i = 0; i < 10; i++)
-        {
-            seatViewsMap.put(i, new SeatViews());
-            seatViewsMap.get(i).edtxtPlayerName = findViewById(getResources().getIdentifier("seat_" + String.valueOf(i), "id", getPackageName()));
-            seatViewsMap.get(i).txtChipsTotal = findViewById(getResources().getIdentifier("txt_chips_" + String.valueOf(i), "id", getPackageName()));
-            seatViewsMap.get(i).txtRoundChipsBetted = findViewById(getResources().getIdentifier("txt_bet_" + String.valueOf(i), "id", getPackageName()));
-            seatViewsMap.get(i).txtRoundRole = findViewById(getResources().getIdentifier("txt_role_" + String.valueOf(i), "id", getPackageName()));
-        }
+        players = game.getTable().getPlayers();
 
-        ArrayList<Player> players = game.getTable().getPlayers();
-        // Inicialização das views com os valores
-        for(int i = 0; i < 10; i++)
-        {
-            if(players.get(i) != null)
-            {
-                seatViewsMap.get(i).edtxtPlayerName.setText((CharSequence) players.get(i).getName());
-                seatViewsMap.get(i).txtChipsTotal.setText((CharSequence) String.valueOf(players.get(i).getChips()));
-            }
-        }
+        startGameActivity();
         
         // Botão de call ou check
         Button btnCheckCall = findViewById(R.id.btn_check_call);
@@ -93,5 +80,62 @@ public class GameActivity extends AppCompatActivity {
             DialogFragment dialog = new raiseDialog();
             dialog.show(getSupportFragmentManager(), "Raise");
         });
+    }
+
+    // Inicializa as informações das views
+    @SuppressLint("UseCompatLoadingForColorStateLists")
+    private void startGameActivity()
+    {
+        // Faz o bind de todas as views e configura seus valores
+        for(int i = 0; i < 10; i++)
+        {
+            seatViewsMap.put(i, new SeatViews());
+            seatViewsMap.get(i).edtxtPlayerName = findViewById(getResources().getIdentifier("seat_" + String.valueOf(i), "id", getPackageName()));
+            seatViewsMap.get(i).txtChipsTotal = findViewById(getResources().getIdentifier("txt_chips_" + String.valueOf(i), "id", getPackageName()));
+            seatViewsMap.get(i).txtRoundChipsBetted = findViewById(getResources().getIdentifier("txt_bet_" + String.valueOf(i), "id", getPackageName()));
+            seatViewsMap.get(i).txtRoundRole = findViewById(getResources().getIdentifier("txt_role_" + String.valueOf(i), "id", getPackageName()));
+            if(players.get(i) != null)
+            {
+                seatViewsMap.get(i).edtxtPlayerName.setText((CharSequence) players.get(i).getName());
+                seatViewsMap.get(i).txtChipsTotal.setText((CharSequence) String.valueOf(players.get(i).getChips()));
+            }
+        }
+
+        int dealerID = selectDealerID();
+        players.get(dealerID).setDealer(true);
+        seatViewsMap.get(dealerID).txtRoundRole.setText(R.string.role_dealer);
+
+        int smallBlidID = getNextValidPlayer(dealerID);
+        players.get(smallBlidID).setSmallBlind(true);
+        seatViewsMap.get(smallBlidID).txtRoundRole.setText(R.string.role_small_blind);
+
+        int bigBlidID = getNextValidPlayer(smallBlidID);
+        players.get(bigBlidID).setBigBlind(true);
+        seatViewsMap.get(bigBlidID).txtRoundRole.setText(R.string.role_big_blind);
+
+        // Primeiro a jogar
+        int firstFocusedPlayer = getNextValidPlayer(bigBlidID);
+        game.getTable().setFocusedPlayer(firstFocusedPlayer);
+        seatViewsMap.get(firstFocusedPlayer).edtxtPlayerName.setBackgroundTintList(getApplicationContext().getResources().getColorStateList(R.color.turn));
+
+        // # TODO fazer um startGame em Game e chamar aqui
+    }
+
+    private int selectDealerID()
+    {
+        Random rand = new Random();
+        return getNextValidPlayer(rand.nextInt(10));
+    }
+
+    private int getNextValidPlayer(int actualFocusedPlayer)
+    {
+        int nextFocusedPlayer  = actualFocusedPlayer;
+
+        do{
+            nextFocusedPlayer++;
+            if(nextFocusedPlayer == 10) nextFocusedPlayer = 0;
+        }while(players.get(nextFocusedPlayer) == null || players.get(nextFocusedPlayer).isFolded() || !players.get(nextFocusedPlayer).isPlaying());
+
+        return nextFocusedPlayer;
     }
 }
